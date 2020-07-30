b41f5b1246b044baf037b75aa67b0764ad4f2851
Mehedi Zamadar <meheditcf@gmail.com>
2020-07-31 02:20:58 +0600

    creating model and displaying them in components

--- shopping/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe [] = [
+    new Recipe('Icecream', 'How to Create Icecream', 'https://minimalistbaker.com/wp-content/uploads/2015/06/AMAZING-Vegan-Cherry-Pie-ICE-CREAM-10-ingredients-simple-methods-SO-delicious-vegan-recipe-icecream-dessert-cherry-summer-fruit-675x1024-1.jpg'),
+    new Recipe('Chocholate', 'How to Create Chocholate', 'https://minimalistbaker.com/wp-content/uploads/2015/06/AMAZING-Vegan-Cherry-Pie-ICE-CREAM-10-ingredients-simple-methods-SO-delicious-vegan-recipe-icecream-dessert-cherry-summer-fruit-675x1024-1.jpg')
+
+  ];
+
 
   constructor() { }
 
