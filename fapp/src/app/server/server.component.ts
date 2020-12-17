0772a7e362dd69d3cb5adfd20af38f79ba321269
Mehedi Zamadar <meheditcf@gmail.com>
2020-12-17 17:17:03 +0600

    property binding, string interpolation, directives recap

--- fapp/src/app/server/server.component.ts
+++ fapp/src/app/server/server.component.ts
@@ -9,12 +9,20 @@ export class ServerComponent {
     server: string ='server011';
     serverId: number = 10;
     serverStatus: string ='Offline';
+    createServer: boolean = false;
     constructor(){
         this.serverStatus = Math.random() > 0.5? 'Online': 'Offline';
+        setTimeout(()=>{
+            this.createServer = true;
+
+        }, 2000)
     }
 
     getServerStatus(){
         return this.serverStatus;
+    }
+    getColor(){
+        return this.serverStatus === 'Online' ? 'green' : 'red';
     }
 
     
