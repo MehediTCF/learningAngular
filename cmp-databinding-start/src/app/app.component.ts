e9fec1826fcd8dd69525e3ccc3b149056a8e3112
Mehedi Zamadar <meheditcf@gmail.com>
2021-04-17 23:43:04 +0600

    using eventemitter and data passing from inner to outer component

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'S1', content: 'How r ya'}, {type: 'blueprint', name: 'S1', content: 'How r ya'}];
+  serverElements = [{type: 'server', name: 'S1', content: 'How r ya'}, {type: 'blueprint', name: 'S2', content: 'How r ya22'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+
+    });
+  }
 
 }
